393f631375014683b9163c215cfbe7fcd8aed380
Johnny Magrippis <j@magrippis.com>
2023-02-25 16:10:20 +0100

    ⚡️  add rudimentary private cache

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,11 +5,16 @@ export type Theme = 'light' | 'dark' | 'auto'
 export const isValidTheme = (theme: FormDataEntryValue | null): theme is Theme =>
 	!!theme && (theme === 'light' || theme === 'dark' || theme === 'auto')
 
+const FIVE_MINUTES_IN_SECONDS = 5 * 60
+
 export const handle: Handle = async ({event, resolve}) => {
 	const theme = event.cookies.get('theme') ?? 'auto'
 	if (isValidTheme(theme)) {
 		event.locals.theme = theme
 	}
+	event.setHeaders({
+		'cahce-control': `private, max-age=${FIVE_MINUTES_IN_SECONDS}`,
+	})
 	const response = await resolve(event, {
 		transformPageChunk: ({html}) => html.replace('%THEME%', theme),
 	})
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import type {PageServerData} from './$types'
+
+	export let data: PageServerData
+</script>
+
 <div
 	class="flex h-[80vh] shrink-0 flex-col items-center justify-center bg-primary-100 p-4 dark:bg-primary-900"
 >
@@ -7,25 +13,17 @@
 <section class="container py-4 px-2">
 	<h2 class="mb-4 text-4xl">Popular playlists</h2>
 	<ul class="grid gap-4 lg:grid-cols-2">
-		<li>
-			<iframe
-				class="aspect-video w-full"
-				src="https://www.youtube-nocookie.com/embed/videoseries?list=PLfiNMmap-wax0IXgCOuaMgwua20JTjphC"
-				title="Relaxing Waves YouTube Playlist"
-				frameborder="0"
-				allow="clipboard-write; encrypted-media; picture-in-picture;"
-				allowfullscreen
-			/>
-		</li>
-		<li>
-			<iframe
-				class="aspect-video w-full"
-				src="https://www.youtube-nocookie.com/embed/videoseries?list=PLfiNMmap-wawv9pcU0fKAcCbO2_f0QHzf"
-				title="Relaxing Waves YouTube Playlist"
-				frameborder="0"
-				allow="clipboard-write; encrypted-media; picture-in-picture;"
-				allowfullscreen
-			/>
-		</li>
+		{#each data.playlists as { src, title }}
+			<li>
+				<iframe
+					class="aspect-video w-full"
+					{src}
+					{title}
+					frameborder="0"
+					allow="clipboard-write; encrypted-media; picture-in-picture;"
+					allowfullscreen
+				/>
+			</li>
+		{/each}
 	</ul>
 </section>
