3b726d076ce1b48c2cc4e1af182e83692bf1ae65
Johnny Magrippis <j@magrippis.com>
2023-02-25 16:17:21 +0100

    🔍 Basic meta titles and descriptions

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,9 @@
 import type {LayoutServerLoad} from './$types'
 
-export const load: LayoutServerLoad = ({locals}) => ({theme: locals.theme})
+export const load: LayoutServerLoad = ({locals}) => ({
+	theme: locals.theme,
+	defaultMeta: {
+		title: 'Chill & get inspired!',
+		description: 'Time-lapses, drone footage & hype videos from all around the world!',
+	},
+})
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,26 @@
 	import '@fontsource/noto-color-emoji'
 
 	import '../app.css'
+	import type {LayoutServerData} from './$types'
+	import {page} from '$app/stores'
 	import Header from '$lib/components/Header/Header.svelte'
 	import Footer from '$lib/components/Footer/Footer.svelte'
+	export let data: LayoutServerData
+
+	$: title = $page.data.meta?.title ?? data.defaultMeta.title
+	$: description = $page.data.meta?.description ?? data.defaultMeta.description
 </script>
+
+<svelte:head>
+	<title>{title} | Moving Scapes</title>
+	<meta name="description" content={description} />
+
+	<meta property="og:title" content={title} />
+	<meta property="og:type" content="article" />
+
+	<meta property="og:description" content={description} />
+	<meta property="og:site_name" content="Moving Scapes" />
+</svelte:head>
 
 <Header />
 <slot />
