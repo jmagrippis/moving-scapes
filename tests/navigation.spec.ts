6823b994c395a4d00c82690884e3468fadf141cf
Johnny Magrippis <j@magrippis.com>
2023-03-27 17:12:30 +0200

    ⬆️ upgrade dependencies

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -4,12 +4,12 @@ test('navigation smoke test', async ({page}) => {
 	// start at the home page
 	await page.goto('/')
 
-	await expect(page).toHaveTitle(/Moving Scapes/)
 	await expect(page.locator('role=heading[level=1]')).toHaveText('Moving Scapes')
+	await expect(page).toHaveTitle(/Moving Scapes/)
 
 	// navigate to the About Page
 	await page.locator('role=link[name=About]').click()
 
-	await expect(page).toHaveTitle(/About/)
 	await expect(page.locator('role=heading[level=1]')).toHaveText('About our project')
+	await expect(page).toHaveTitle(/About/)
 })
